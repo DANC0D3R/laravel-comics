c2a45ffc0aa2c9c8caba5015e41deda1b1ab1be0
Daniele Minieri <danieleminieri99@gmail.com>
2023-03-09 20:22:42 +0100

    Esercizio completato

---
+++ resources/views/Includes/footer.blade.php
@@ -0,0 +1,110 @@
+<footer>
+    <!-- Blu Menu -->
+    <div class="blue-menu">
+        <div class="container">
+            <ul>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                        <span>dc digital comics</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                        <span>dc merchandise</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                        <span>subscription</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                        <span>comic shop locator</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                        <span>dc power visa</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    {{-- LINKS --}}
+    <div class="jumbo-bg">
+        <div class="container">
+            <div class="d-flex">
+                <div class="col">
+                    <h3>DC Comics</h3>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <h3>Shop</h3>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>DC</h3>
+                    <ul>
+                        <li>Terms of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subcriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>Sites</h3>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <figure>
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="DC">
+                </figure>
+            </div>
+        </div>
+    </div>
+    {{-- SOCIAL --}}
+    <div class="gray-bg">
+        <div class="container">
+            <button>SIGN-UP NOW!</button>
+            <div>
+                <a href="#">FOLLOW US</a>
+                <ul>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest"></a></li>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/Includes/header.blade.php
@@ -0,0 +1,45 @@
+<header>
+    <div class="container">
+        {{-- LOGO --}}
+        <figure>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC">
+            </a>
+        </figure>
+        {{-- NAVBAR --}}
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Collectibles</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">Fans</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+                <li>
+                    <a href="#">Shop</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
